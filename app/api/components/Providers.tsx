4176dd90e2472925468dbddfc3beeab9ebbe7c5d
Praveen <praying.sharma@gmail.com>
2025-03-12 22:01:31 +0530

    Add

--- app/api/components/Providers.tsx
+++ app/api/components/Providers.tsx
@@ -2,10 +2,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { ImageKitProvider } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
 
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
-const urlEndpoint=process.env.NEXT_PUBLIC_URL_ENDPOINT;
-const publicKey=process.env.NEXT_PUBLIC_PUBLIC_KEY;
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const authenticator = async () => {
+    try {
+      const response = await fetch("/api/imagekit-auth");
 
-
-const authenti
+      if (!response.ok) {
+        throw new Error("Failed to authenticate with ImageKit");
+      }
+      const data = await response.json();
+      const { signature, expire, token } = data;
+      return { signature, expire, token };
+    } catch (error) {
+      console.error("Error authenticating with ImageKit:", error);
+      throw error;
+    }
+  };
+  return (
+    <SessionProvider>
+      <ImageKitProvider
+        urlEndpoint={urlEndpoint}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        {children}
+      </ImageKitProvider>
+    </SessionProvider>
+  );
+}
